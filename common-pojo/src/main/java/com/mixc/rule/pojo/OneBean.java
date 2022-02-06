39dfdd48cff23121d4f4cf30e89b64fe961a80ea
dexter yang <dangeryyy@126.com>
2022-02-06 21:39:02 +0800

    init

---
+++ common-pojo/src/main/java/com/mixc/rule/pojo/OneBean.java
@@ -0,0 +1,10 @@
+package com.mixc.rule.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class OneBean {
+    private String name;
+}
---
+++ event-dealer/src/main/java/com/mixc/rulengine/Main.java
@@ -0,0 +1,11 @@
+package com.mixc.rulengine;
+
+import com.mixc.rule.pojo.OneBean;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = new String();
+        OneBean a = new OneBean("Dexter");
+        System.out.println(s + "Hello" +a);
+    }
+}
